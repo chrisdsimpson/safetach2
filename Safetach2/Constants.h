e471cfd1da16dc549fb93747857cc4207d64083b
chrisdsimpson <chrisdsimpson@yahoo.com>
2017-06-05 16:41:44 -0700

    Added new defines

--- Safetach2/Constants.h
+++ Safetach2/Constants.h
@@ -40,26 +40,34 @@
 #define Constants_h
 
 /* State defines */
-#define MENU_STATE_HOME             1
-#define MENU_STATE_GRAPH            2
-#define MENU_STATE_AUDIO            3
+#define MENU_STATE_HOME                 1
+#define MENU_STATE_GRAPH                2
+#define MENU_STATE_AUDIO                3
 
-#define SUB_MENU_STATE_VELOCITY     1
-#define SUB_MENU_STATE_ACCELERATION 2
-#define SUB_MENU_STATE_JERK         3
-#define SUB_MENU_STATE_AUDIO_LEVEL  4
-#define SUB_MENU_STATE_AUDIOFREQ    5
+#define SUB_MENU_STATE_VELOCITY         1
+#define SUB_MENU_STATE_ACCELERATION     2
+#define SUB_MENU_STATE_JERK             3
+#define SUB_MENU_STATE_AUDIO_LEVEL      4
+#define SUB_MENU_STATE_AUDIOFREQ        5
 
 
 /* Color defines */
-#define ColorYellow                 colorWithRed:(250.0/255.0) green:(217.0/255.0) blue:(5.0/255.0) alpha:(1.0)
-#define ColorBlue                   colorWithRed:(27.0/255.0) green:(120.0/255.0) blue:(250.0/255.0) alpha:(1.0)
-#define ColorRed                    colorWithRed:(255.0/255.0) green:(0.0/255.0) blue:(0.0/255.0) alpha:(1.0)
-#define ColorGreen                  colorWithRed:(1.0/255.0) green:(161.0/255.0) blue:(1.0/255.0) alpha:(1.0)
-#define ColorGrey                   colorWithRed:(167.0/255.0) green:(166.0/255.0) blue:(166.0/255.0) alpha:(1.0)
-#define ColorMagenta                colorWithRed:(225.0/255.0) green:(0.0/255.0) blue:(255.0/255.0) alpha:(1.0)
-#define ColorLightGrey              colorWithRed:(217.0/255.0) green:(217.0/255.0) blue:(217.0/255.0) alpha:(1.0)
-#define ColorOrange                 colorWithRed:(255.0/255.0) green:(153.0/255.0) blue:(51.0/255.0) alpha:(1.0)
+#define ColorYellow                     colorWithRed:(250.0/255.0) green:(217.0/255.0) blue:(5.0/255.0) alpha:(1.0)
+#define ColorBlue                       colorWithRed:(27.0/255.0) green:(120.0/255.0) blue:(250.0/255.0) alpha:(1.0)
+#define ColorRed                        colorWithRed:(255.0/255.0) green:(0.0/255.0) blue:(0.0/255.0) alpha:(1.0)
+#define ColorGreen                      colorWithRed:(1.0/255.0) green:(161.0/255.0) blue:(1.0/255.0) alpha:(1.0)
+#define ColorGrey                       colorWithRed:(167.0/255.0) green:(166.0/255.0) blue:(166.0/255.0) alpha:(1.0)
+#define ColorMagenta                    colorWithRed:(225.0/255.0) green:(0.0/255.0) blue:(255.0/255.0) alpha:(1.0)
+#define ColorLightGrey                  colorWithRed:(217.0/255.0) green:(217.0/255.0) blue:(217.0/255.0) alpha:(1.0)
+#define ColorOrange                     colorWithRed:(255.0/255.0) green:(153.0/255.0) blue:(51.0/255.0) alpha:(1.0)
+
+
+/* BLE service and char defines */
+#define TRANSFER_SERVICE_UUID           @"FB694B90-F49E-4597-8306-171BBA78F846"
+#define TRANSFER_CHARACTERISTIC_UUID    @"EB6727C4-F184-497A-A656-76B0CDAC633A"
+
+#define NOTIFY_MTU                      20
+
 
 
 
